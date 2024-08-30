3a5468ce9b560ccd61e5a3c83d16e73f15ce13de
limon <sabur.islam@sec.ac.bd>
2024-08-30 20:52:15 +0600

    FEAT: Reverse Porxy added 1. made gate way proxy complaint 2. public and private api is managed seperately

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-const {server_config,Logger} = require('./config');
+const { server_config, Logger } = require('./config');
 const express = require('express');
 const apiGateWayRoutes = require('./routes');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 
+app.use(
+    '/safar/public',
+    createProxyMiddleware({
+        target: 'http://localhost:3002/api/public',
+        changeOrigin: true,
+        pathRewrite:{'^/safar/public':'/'}
+    }),
+);
+
+app.use(
+    '/safar/private',
+    createProxyMiddleware({
+        target: 'http://localhost:3002/api/private',
+        changeOrigin: true,
+        pathRewrite:{'^/safar/private':'/'}
+    }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api.safar',apiGateWayRoutes);
+app.use('/api', apiGateWayRoutes);
 
-app.listen(server_config.PORT, ()=>{
+app.listen(server_config.PORT, () => {
     console.log(`Server Started at ${server_config.PORT}`);
     Logger.log({
-        level:'info',
-        message:'Server Up and Running!',
-        label:__filename,
-        errors:{msg:'something'}
+        level: 'info',
+        message: 'Server Up and Running!',
+        label: __filename,
+        errors: { msg: 'something' }
     });
 });
